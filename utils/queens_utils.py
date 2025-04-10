e1fccf8698d8436fc852c4e8013e89387b65df51
Mahantesh Hiremath <mahanteshimath@gmail.com>
2025-04-10 21:05:16 +0530

    N-Queen fixed

--- utils/queens_utils.py
+++ utils/queens_utils.py
@@ -11,7 +11,59 @@ class NQueensSolver:
         self.temperature_history = []
         self.energy_history = []
     
-    # ... existing initialization and helper methods ...
+    def initialize_random_state(self):
+        """Initialize board with random queen positions (one per row)"""
+        # Create empty board
+        self.board = np.zeros((self.n, self.n))
+        # Place queens randomly, one per row
+        for i in range(self.n):
+            j = random.randint(0, self.n-1)
+            self.board[i][j] = 1
+        self.current_conflicts = self.count_conflicts()
+    
+    def count_conflicts(self):
+        """Count number of queen pairs that can attack each other"""
+        conflicts = 0
+        queen_positions = self.get_queen_positions()
+        
+        for i in range(len(queen_positions)):
+            for j in range(i + 1, len(queen_positions)):
+                if self.can_attack(queen_positions[i], queen_positions[j]):
+                    conflicts += 1
+        return conflicts
+    
+    def get_queen_positions(self):
+        """Return list of (row, col) positions of all queens"""
+        return [(row, col) for row in range(self.n) 
+                for col in range(self.n) if self.board[row][col] == 1]
+    
+    def can_attack(self, pos1, pos2):
+        """Check if two queens at given positions can attack each other"""
+        row1, col1 = pos1
+        row2, col2 = pos2
+        return (col1 == col2 or  # Same column
+                abs(row1 - row2) == abs(col1 - col2))  # Same diagonal
+    
+    def make_random_move(self):
+        """Make a random move by moving a random queen to a new position in its row"""
+        row = random.randint(0, self.n-1)
+        old_col = np.where(self.board[row] == 1)[0][0]
+        
+        # Try moving queen to a new position in the same row
+        new_col = random.randint(0, self.n-1)
+        while new_col == old_col:
+            new_col = random.randint(0, self.n-1)
+        
+        # Make the move
+        new_board = self.board.copy()
+        new_board[row][old_col] = 0
+        new_board[row][new_col] = 1
+        
+        # Calculate new conflicts
+        self.board = new_board
+        new_conflicts = self.count_conflicts()
+        
+        return new_board, new_conflicts
     
     def simulated_annealing(self, initial_temp=10.0, cooling_rate=0.95, min_temp=0.01, max_iterations=10000):
         """Solve N-Queens using simulated annealing with improved visualization"""
