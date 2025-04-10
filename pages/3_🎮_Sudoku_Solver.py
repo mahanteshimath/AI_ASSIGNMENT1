a3c0974bbf93cbfd2fc7d61ff5739c8bb84ea52c
Mahantesh Hiremath <mahanteshimath@gmail.com>
2025-04-10 22:07:42 +0530

    Final Done

--- pages/3_🎮_Sudoku_Solver.py
+++ pages/3_🎮_Sudoku_Solver.py
@@ -11,6 +11,11 @@ from utils.sudoku_utils import (
 )
 
 st.set_page_config(page_title="Sudoku Solver", page_icon="🎮", layout="wide")
+st.logo(
+    image="https://upload.wikimedia.org/wikipedia/en/4/41/Flag_of_India.svg",
+    link="https://www.linkedin.com/in/mahantesh-hiremath/",
+    icon_image="https://upload.wikimedia.org/wikipedia/en/4/41/Flag_of_India.svg"
+)
 
 st.logo(
     image="https://upload.wikimedia.org/wikipedia/en/4/41/Flag_of_India.svg",
